5405ae6881fc59d2d6c945c65013c1450599298f
Nikita <mynewacc.create@gmail.com>
2020-05-19 23:49:32 +0300

    feat: implement ChatViewServlet servlet

---
+++ Chat/src/servlet/ChatViewServlet.java
@@ -0,0 +1,39 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import entity.ChatUser;
+
+public class ChatViewServlet extends ChatServlet {
+	
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setCharacterEncoding("utf8");
+		boolean b=false;
+		for (ChatUser aUser: activeUsers.values()) {
+			if(aUser.getName().equals((String)request.getSession().getAttribute("name"))){
+				b=true;
+			}
+		}
+		if(!b)
+			response.sendRedirect(response.encodeRedirectURL("/lab8_1/login.do"));
+		PrintWriter pw = response.getWriter();
+		pw.println("<!DOCTYPE html>\n<html>" +
+					"<head>" +
+						"<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>" +
+						"<title>×àò</title>" +
+					"</head>");
+
+		pw.println( "<frameset rows=\"900,10\">" +
+						"<frameset cols=\"5,2\">"+
+							"<frame name=\"frameMessages\" src=\"/lab8_1/message.do\">"+
+							"<frame name=\"frameUsers\" src=\"/lab8_1/users.do\" noresize>" +
+							"</frameset>"+
+					"</frameset>");
+		pw.println("</html>");
+	}
+}
